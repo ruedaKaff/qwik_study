7a4f212d322810df58b18f2749fecef0022363e5
ruedaKaff <jhanru@live.com>
2023-08-12 17:13:30 -0500

    understanding signals and tailwinds styles for dimanyc count

--- qwik-Poke/src/routes/index.tsx
+++ qwik-Poke/src/routes/index.tsx
@@ -1,21 +1,28 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-
 export default component$(() => {
+  const pokemonId = useSignal(1); //Primitivos , para complejos y coleeciones useStore
+
   return (
     <>
-      <span class="text-5xl" >Hello World!</span>
+      <span class="text-2xl">Buscador simple</span>
+      <span class="text-8xl">{pokemonId.value + 1}</span>
+      {/* TODO: crear imagen del pokemon */}
+      <div class="mt-2">
+        <button class="btn btn-primary mr-2">Next </button>
+        <button class="btn btn-primary">Previous</button>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Poke Qwik",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
-    }, 
+      content: "Qwik site for first time ",
+    },
   ],
 };
