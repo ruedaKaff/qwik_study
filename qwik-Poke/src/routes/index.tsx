30ea7c96a3e1feb0c45c3870b181788108d69f07
ruedaKaff <jhanru@live.com>
2023-08-13 12:40:43 -0500

    Navigating by id and changin url to turn back pokemon

--- qwik-Poke/src/routes/index.tsx
+++ qwik-Poke/src/routes/index.tsx
@@ -1,21 +1,42 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { $,component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { PokemonImage } from "~/components/pokemon/pokemon-image";
+
 
 export default component$(() => {
   const pokemonId = useSignal(1); //Primitivos , para complejos y coleeciones useStore
+  const showBack = useSignal(false);
+
+  const changePokemonID = $(( value:number ) =>{
+    if ( (pokemonId.value + value ) <= 0 ) return;
+
+    pokemonId.value += value;
+
+  })
+ 
+
 
   return (
+
     <>
       <span class="text-2xl">Buscador simple</span>
-      <span class="text-8xl">{pokemonId.value + 1}</span>
-      {/* TODO: crear imagen del pokemon */}
-      <div class="mt-2">
-        <button class="btn btn-primary mr-2">Next </button>
-        <button class="btn btn-primary">Previous</button>
+      <span class="text-8xl">{pokemonId}</span>
+      
+      
+      <PokemonImage  id={pokemonId.value} backImage={showBack.value}></PokemonImage>
+       
+      <div class="mt-8">
+        <button onClick$={()=> changePokemonID(-1)} class="btn btn-primary mr-2">Previous </button>  
+
+        <button onClick$={()=> changePokemonID(+1)} class="btn btn-primary mr-2">Next</button>
+
+        <button onClick$={()=> showBack.value = !showBack.value } class="btn btn-primary">Turn back</button>
       </div>
     </>
   );
+  
 });
+
 
 export const head: DocumentHead = {
   title: "Poke Qwik",
